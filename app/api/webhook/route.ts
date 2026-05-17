4ad0410eb06cfb8597cb3045dbe73471936293db
Rocky Hollen <rocky@hollenholdings.com>
2026-05-17 02:30:30 -0700

    fix stripe api version

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -9,7 +9,7 @@ import Stripe from 'stripe'
 import { supabaseAdmin } from '@/lib/supabase'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
+  apiVersion: '2025-08-27.basil',
 })
 
 export async function POST(req: NextRequest) {
